9882a7b0f30bb1b73d9420e0a8d535360f7f7c3d
Enrico <erossini@vubiquity.com>
2020-03-11 16:02:31 +0000

    Add history and navigation

--- 10-TwitchTV/streams/client/src/reducers/streamReducer.js
+++ 10-TwitchTV/streams/client/src/reducers/streamReducer.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import {
     CREATE_STREAM,
     FETCH_STREAM,
-    FETCH_STREAM,
     EDIT_STREAM,
     DELETE_STREAM,
     FETCH_STREAMS
@@ -10,6 +9,9 @@ import {
 
 export default (state = {}, action) => {
     switch(action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id')};
+
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
 
