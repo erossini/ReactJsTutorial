221632292d6a453ede4fccfe6a648a6f54d2f846
Enrico <erossini@vubiquity.com>
2020-03-12 16:21:50 +0000

    Add React Router

--- 10-TwitchTV/streams/client/src/components/streams/StreamList.js
+++ 10-TwitchTV/streams/client/src/components/streams/StreamList.js
@@ -12,9 +12,7 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
-                        Edit
-                    </button>
+                    <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>Edit</Link>
                     <button className="ui button negative">
                         Delete
                     </button>
@@ -44,7 +42,7 @@ class StreamList extends React.Component {
         if (this.props.isSignedIn) {
             return (
                 <div style={{ textAlign: 'right' }}>
-                    <Link to="/stream/new" className="ui button primary">
+                    <Link to="/streams/new" className="ui button primary">
                     Create Stream
                     </Link>
                 </div>
