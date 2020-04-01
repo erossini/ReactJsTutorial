95324f35db2f72763a66e3571eff9283a4189871
Enrico Rossini <enrico.rossini.uk@live.com>
2020-04-01 16:49:39 +0100

    Complete basic CRUD application

--- 10-TwitchTV/streams/client/src/components/streams/StreamList.js
+++ 10-TwitchTV/streams/client/src/components/streams/StreamList.js
@@ -13,9 +13,7 @@ class StreamList extends React.Component {
             return (
                 <div className="right floated content">
                     <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>Edit</Link>
-                    <button className="ui button negative">
-                        Delete
-                    </button>
+                    <Link className="ui button negative" to={`/streams/delete/${stream.id}`}>Delete</Link>
                 </div>
             );
         }
@@ -28,7 +26,9 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header">
+                            {stream.title}
+                        </Link>
                         <div className="description">
                             {stream.description}
                         </div>
